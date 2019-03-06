1b3e38e5827913241e40253d4fbbf62cd70dc32f
Brian Simonton <bsimonton78@gmail.com>
2019-03-06 03:15:04 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -4,3 +4,19 @@
 # or whitespace (' ').
 
 
+class EmailParser
+
+  attr_accessor :email
+  
+  
+  def  initialize(emails)
+    @email = emails
+  end
+
+  def parse
+    email_array = @email.split(/[, ]/).uniq
+    email_array.reject! {|element| element.empty?}
+    email_array
+  end
+
+end
